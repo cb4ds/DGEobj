d0bed39176eb08bcb3b360d1aeabf4072c830b9c
agenius-lisa <lisacao@ip-172-16-12-101.ca-central-1.compute.internal>
2022-02-18 03:58:20 -0800

    add item manipulation tests, modify attributes test

--- tests/testthat/test_reset.R
+++ tests/testthat/test_reset.R
@@ -8,6 +8,31 @@ test_that('reset.R: ', {
     # compare objects and classes
     expect_s3_class(new, "DGEobj")
     expect_equivalent(new, t_obj)
+    expect_equivalent(inventory(new), inventory(t_obj))
+
+
+    ## TEST DATA ITEM MANIPULATION - test that reset reverts all these changes
+    # manipulations
+    # testing t_obj with new type
+    test_t_obj <- newType(t_obj, "MyType", "meta")
+    test_t_obj <- resetDGEobj(test_t_obj) # should this be removing the type?
+    expect_identical(showTypes(test_t_obj)$Type, showTypes(t_obj)$Type)
+
+
+    # testing t_obj with add item
+    test_t_obj <- addItem(t_obj,
+                   item = 'Fred Flintstone',
+                   itemName = 'Cartoon',
+                   itemType = 'meta',
+                   itemAttr = list('MyAttribute' = 'testObject'))
+    expect_equivalent(resetDGEobj(test_t_obj), t_obj) # Not passing - needs refinement
+    expect_identical(resetDGEobj(new), test_t_obj) # need to omit "dateCreated" attribute - noisey
+
+    # testing t_obj with rm item
+    test_t_obj <- rmItem(t_obj, "design")
+    expect_equivalent(resetDGEobj(test_t_obj), t_obj) # Not passing - needs refinement
+    expect_identical(resetDGEobj(new), test_t_obj) # need to omit "dateCreated" attribute - noisey
+
 
     ## TEST DIMENSIONS - test that dimension and data organization changes are reverted
     # test that dimensions, row/col names are returned to original state after change
@@ -16,13 +41,23 @@ test_that('reset.R: ', {
     expect_setequal(names(new), c("counts_orig", "counts", "design_orig", "design",
                                   "geneData_orig", "geneData", "granges_orig", "granges"))
 
+    expect_named(new, c("counts_orig", "counts", "design_orig", "design",
+                        "geneData_orig", "geneData", "granges_orig", "granges"))
+
     expect_equal(dim(new), c(1000, 48))
 
     expect_equal(dimnames(new), dimnames(t_obj)) # refine this with actual output
 
 
     ## TEST ATTRIBUTES - test attributes are reset back to original after changes
-    # set attributes to NULL
+
+    # testing t_obj with new attributes
+    new_attributes <- list("attribute1" = runif(100, min = 0, max = 2), "attribute2" = LETTERS)
+    test_t_obj <- setAttributes(t_obj, new_attributes)
+
+    expect_equivalent(getAttributes(resetDGEobj(test_t_obj)), getAttributes(t_obj))
+
+    # testing t_obj with attributes set to NULL
     test_t_obj  <- t_obj
     test_t_obj[] <- lapply(test_t_obj,
                            function(x) {
@@ -31,7 +66,7 @@ test_that('reset.R: ', {
                  regexp = "primaryAssayData must be specified as a matrix or a data.frame.",
                  fixed = TRUE)
 
-    # set to attribute values to NULL
+    # testing t_obj with attribute values set to NULL
     test_t_obj <- lapply(attributes(test_t_obj), function(x) {
         attr(x, "label") <- NULL
         attr(x, "labels") <- NULL})
@@ -56,5 +91,7 @@ test_that('reset.R: ', {
     test_t_obj1 <- setAttributes(t_obj, list("PlatformType" = "fred"))
     expect_error(resetDGEobj(test_t_obj1),
                  regexp = "The PlatformType attribute value was not recognized!")
+
+
 
 })
