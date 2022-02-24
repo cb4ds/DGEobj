a1c43ec55e432d1954b1961bc1ecfdd50d1e7fe5
agenius-lisa <lisacao@ip-172-16-12-101.ca-central-1.compute.internal>
2022-02-24 14:54:45 -0800

    add finished gene obj test

--- tests/testthat/test_reset.R
+++ tests/testthat/test_reset.R
@@ -5,53 +5,70 @@ test_that('reset.R: ', {
 
     new <- resetDGEobj(t_obj)
 
-    gene_obj <-  readRDS(system.file("exampleObj.RDS", package = "DGEobj", mustWork = TRUE))
-    isoform_obj <- "placeholder_value"
-    protein_obj <- "placeholder_value"
-    exon_obj <- "placeholder_value"
-
+    # The following are defined in setup.R: gene_data, isoform_data, protein_data, exon_data
 
     # test gene level data
-    test_gene_obj <- function(gene_obj) {
+    test_gene_obj <- function(gene_data) {
 
-        test_t_obj <- initDGEobj(primaryAssayData = t_obj$counts,
-                                 rowData = t_obj$geneData,
-                                 colData = t_obj$design,
-                                 level = "gene")
-
+        test_t_obj <- gene_data
+        test_t_reset <- resetDGEobj(test_t_obj)
 
         # object validation
-        expect_s3_class(test_t_obj, "DGEobj")
+        expect_s3_class(test_t_reset, "DGEobj")
+        expect_equivalent(showMeta(test_t_reset), showMeta(gene_data))
+
+        # validate level is gene before and after reset
+        test_t_meta <- showMeta(test_t_obj)
+        expect_equal("gene", test_t_meta$Value[3])
+        expect_equal(attr(test_t_obj, "level"), "gene")
+
+        test_t_meta_reset <- showMeta(test_t_reset)
+        expect_equal("gene", test_t_meta_reset$Value[3])
+        expect_equal(attr(test_t_reset, "level"), "gene")
+
+        # reset after subsetting
+        test_t_dim <- dim(test_t_obj)
+        expect_equal(dim(test_t_obj),c(951, 48))
+        test_t_obj <- test_t_obj[c(1:10), ]
+        test_t_reset <- resetDGEobj(test_t_obj)
+        expect_equal(dim(test_t_reset), c(1000, 48))
+
+        # check names after reset
+        expect_named(test_t_obj, c('counts_orig', 'counts', 'design_orig', 'design', 'geneData_orig',
+                                   'geneData', 'granges_orig', 'granges', 'DGEList', 'ReplicateGroupDesign',
+                                   'ReplicateGroupDesign_Elist', 'ReplicateGroupDesign_fit',
+                                   'ReplicateGroupDesign_fit_cm', 'ReplicateGroupDesign_fit_cf',
+                                   'BDL_vs_Sham', 'EXT1024_vs_BDL', 'Nint_vs_BDL', 'Sora_vs_BDL'))
+        expect_named(test_t_reset, c("counts_orig", "counts", "design_orig", "design",
+                                   "geneData_orig", "geneData", "granges_orig", "granges"))
 
 
-        # validate level is gene
-        level_check <- function(test_t_obj) {
-            test_t_meta <- showMeta(test_t_obj)
-            expect_equal("gene", test_t_meta$Value[3])
-            expect_equal(attr(test_t_obj, "level"), "gene") # temp
-            expect_named(test_t_obj, c("counts_orig", "counts", "design_orig", "design",
-                                       "geneData_orig", "geneData", "granges_orig", "granges")) }
+        # testing t_obj with rm item
+        test_t_obj <- rmItem(test_t_obj, "design")
+        expect_false("design" %in% names(test_t_obj))
+        test_t_reset <- resetDGEobj(test_t_obj)
+        expect_false("design" %in% names(test_t_reset)) # check if persists
 
-        level_check(test_t_obj)
+        # testing t_obj with add item
+        test_t_obj <- addItem(test_t_obj,
+                              item = 'Fred Flintstone',
+                              itemName = 'Cartoon',
+                              itemType = 'meta',
+                              itemAttr = list('MyAttribute' = 'testObject'))
+        expect_true("Cartoon" %in% names(test_t_obj))
+        test_t_reset <- resetDGEobj(test_t_obj)
+        expect_false("Cartoon" %in% names(test_t_reset)) # check if removed
 
-        # changes
-        #add rm attribute- should persist
-        #subset
-        #etc
-
-        # change level
-        expect_failure(level_check(test_t_obj))
-
-        # check level is reset
+        # testing t_obj after class change
+        test_t_obj <- as.list.DGEobj(test_t_obj)
+        expect_equal(class(test_t_obj), "list")
+        # coerce back into DGEobj, expect reset to work
+        class(test_t_obj) <- "DGEobj"
         expect_silent(resetDGEobj(test_t_obj))
-
-        # test levels back to normal
-        expect_success(level_check(test_t_obj))
-
     }
 
     #run
-    expect_success(test_gene_obj(gene_obj))
+    expect_success(test_gene_obj(t_obj))
 
     # test isoform level data
     test_isoform_obj <- function(isoform_obj) {
@@ -144,18 +161,6 @@ test_that('reset.R: ', {
                                           colData = t_obj$design,
                                           level = "Fred Flinstone"),
                                           regexp = "The specified level must be one of:  gene, exon, isoform, protein, affy")
-
-    # compare objects and classes
-    expect_s3_class(new, "DGEobj")
-    expect_equivalent(new, t_obj)
-    expect_equivalent(showMeta(new), showMeta(t_obj)) # add more thorough test, check for removals
-
-    # reset subsetting
-    # check dim before
-    expect_equal(t_dim, c(1000, 48))
-    test_t_obj <- t_obj[c(1:10), ]
-    test_t_obj <- resetDGEobj(test_t_obj)
-    expect_equal(dim(test_t_obj), c(1000, 48))
 
     # testing t_obj with new attributes
     new_attributes <- list("attribute1" = runif(100, min = 0, max = 2), "attribute2" = LETTERS)
